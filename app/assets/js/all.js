4d978e36e11fa54d07ea9875f5a0c3a184bc3622
Bruno Yu <jackhellowin@gmail.com>
2022-09-09 16:11:55 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,21 @@
+console.log("Hello!");
+const artistSwiper = new Swiper(".artist__swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  // centeredSlidesBounds:true,
+  initialSlide: 1,
+  spaceBetween: 0,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    // stretch: 0,
+    depth: 200,
+    // modifier: 1,
+    scale: 0.7,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
