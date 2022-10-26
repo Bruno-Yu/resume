c63810d66a77d43ab5358e694375d26074773a81
Bruno Yu <jackhellowin@gmail.com>
2022-10-26 20:36:45 +0800

    Update 2022-10-26T12:36:41.440Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,37 @@
 "use strict";
 
-console.log("Hello!");
+gsap.registerPlugin(ScrollTrigger);
+gsap.to(".stamp", {
+  scrollTrigger: {
+    trigger: ".stamp",
+    start: "center, center",
+    end: "210%, center",
+    toggleAction: "reverse",
+    scrub: true // pin: true,
+    // markers: true
+
+  },
+  rotation: 360,
+  // duration: 15,
+  ease: "sine.inOut",
+  // repeat: -1, 
+  yoyo: true
+});
+var arrAnimate = gsap.to(".arrow", {
+  y: -10,
+  duration: 0.3,
+  yoyo: true,
+  repeat: -1,
+  paused: true
+});
+var arrow = document.querySelector('.arrow');
+arrow.addEventListener('mouseenter', function () {
+  return arrAnimate.play(0);
+});
+arrow.addEventListener('mouseleave', function () {
+  return arrAnimate.pause(0);
+}); // swiper
+
 var artistSwiper = new Swiper(".works__swiper", {
   effect: "coverflow",
   grabCursor: true,
